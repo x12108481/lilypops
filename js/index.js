feaf591c3e17b5b1e414c6994a720babfe6d1384
x12108481 <x12108481@student.ncirl.ie>
2016-11-22 22:37:00 +0000

    Contact page, home page
    adjust main container in all pages.
    added functuality to home page button. add logo js action, adjust form,
    add map

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
+//toogles bootstrap navigation for mobile
 $('.dropdown-toggle').dropdown();
+
+
+//home page See flavours buttom event
+$('#flavours').on('click', function(e) {
+  window.location='products.html';
+});
+
+//on navbar logo click go to Home page
+$('#lilyLogo').on('click', function(e) {
+  window.location='index.html';
+});
