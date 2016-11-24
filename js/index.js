2cf3632c978a17d2fb62465c1dc1f138ddd375cf
x12108481 <x12108481@student.ncirl.ie>
2016-11-24 21:27:45 +0000

    contact us page
    add map, address, chnage background image, change main-container width

--- js/index.js
+++ js/index.js
@@ -11,3 +11,15 @@ $('#flavours').on('click', function(e) {
 $('#lilyLogo').on('click', function(e) {
   window.location='index.html';
 });
+
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 4,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
