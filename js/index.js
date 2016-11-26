5faa7d545b97cc7de97125aad73e3e5d7b857411
x12108481 <x12108481@student.ncirl.ie>
2016-11-26 18:45:03 +0000

    product page improvements
    added image carousel, improved funcionality in js, added button top
    contact page, lists

--- js/index.js
+++ js/index.js
@@ -18,22 +18,19 @@ $('#lilyLogo').on('click', function(e) {
   window.location='index.html';
 });
 
-
-  function initMap() {
-    var haifa = {lat: 32.794, lng: 34.989};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: haifa
-    });
-
-    var marker = new google.maps.Marker({
-      position: haifa,
-      map: map
-    });
-  }
-
+function initMap() {
+  var uluru = {lat: 32.7940, lng: 34.9896};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
 
 $('#submitBtn').on('click',function() {
-   alert("Thanks for contacting us! We will be in touch asap");
-   window.location='index.html';
+  alert("Thanks for contacting us! We will be in touch asap");
+  window.location='index.html';
 });
